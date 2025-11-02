85439a74f9587e23fde5a2a09dab32b78bc96147
Francisco Rivera Viveros <143464458+francisco-rivera-viveros@users.noreply.github.com>
2025-11-01 19:42:08 -0600

    Fix temperature conversion precision in WeatherForecast

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -6,8 +6,9 @@ public class WeatherForecast
 
     public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.555);
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
 }
+
 
